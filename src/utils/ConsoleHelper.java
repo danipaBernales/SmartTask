3da22089c3a0a6b06b0bfe2d3dafadbb9971418e
Daniela Paz Bernales <dbernales.miranda@gmail.com>
2026-04-29 15:32:12 -0400

    Add ConsoleHelper class for title display

---
+++ src/utils/ConsoleHelper.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class ConsoleHelper {
+
+    public static void mostrarTitulo() {
+
+        System.out.println("========================");
+        System.out.println("      SMART TASK        ");
+        System.out.println("========================");
+    }
+}
